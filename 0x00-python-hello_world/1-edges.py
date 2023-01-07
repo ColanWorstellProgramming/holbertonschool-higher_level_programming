2d02120ac3ff8362afcf5c91495e8e8431f9e4e9
Colan Worstell <Cworstellprogramming@gmail.com>
2023-01-07 01:38:11 -0800

    push

--- 0x00-python-hello_world/1-edges.py
+++ 0x00-python-hello_world/1-edges.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 word = "Holberton"
 word_first_3 = "Hol"
-word_last_two = "on"
+word_last_2 = "on"
 middle_word = "olberto"
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
