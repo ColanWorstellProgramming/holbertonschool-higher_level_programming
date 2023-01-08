0fa2a546b7b923f25ac48fc167906bf1d45102c7
Colan Worstell <Cworstellprogramming@gmail.com>
2023-01-08 01:18:27 -0800

    update

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -9,4 +9,4 @@ def uppercase(str):
         if (i != x):
             print("{one}".format(one=chr(c)), end='')
         else:
-            print("{one}\n".format(one=chr(c)))
+            print("{one}".format(one=chr(c)), end=' ')
