a64c7e4015e4642d5296f271f83790562f835116
Colan Worstell <Cworstellprogramming@gmail.com>
2023-01-08 01:02:50 -0800

    test

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,6 +4,6 @@ def uppercase(str):
     for i in range(0, x):
         if (str[i].islower()):
             c = ord(str[i]) - 32
-            print("{one}", end='').format(one=c)
+            print("{one}".format(one=c), end='')
         else:
-            print("{one}", end='').format(one=str[i])
+            print("{one}".format(one=str[i]), end='')
