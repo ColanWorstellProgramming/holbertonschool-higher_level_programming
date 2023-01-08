d52093d7425fdcdd5cbdbab6e28c0bfc24304659
Colan Worstell <Cworstellprogramming@gmail.com>
2023-01-07 20:22:38 -0800

    negative

--- 0x00-python-hello_world/3-last_digit.py
+++ 0x00-python-hello_world/3-last_digit.py
@@ -2,6 +2,8 @@
 import random
 number = random.randint(-10000, 10000)
 num = int(repr(number)[-1])
+if (number < 0):
+    num = -num
 if (num > 5):
     print('Last digit of', number, 'is', num, 'and is greater than 5')
 elif (num == 0):
