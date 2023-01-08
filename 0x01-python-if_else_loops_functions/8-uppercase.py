46803f44c12bbe66f8b5c91951f7a73f3d5e7d14
Colan Worstell <Cworstellprogramming@gmail.com>
2023-01-08 01:56:13 -0800

    update

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,6 +2,6 @@
 def uppercase(str):
     for char in str:
         if ((ord(char) > 96) and (ord(char) < 113)):
-            char = chr(ord(str[i]) - 32)
+            char = chr(ord(char) - 32)
         print("{one}".format(one=char), end='')
     print("")
