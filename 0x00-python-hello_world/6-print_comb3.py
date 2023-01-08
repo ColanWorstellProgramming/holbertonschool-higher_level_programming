1f4259584886b274c04a434aba30e4f6a29db58e
Colan Worstell <Cworstellprogramming@gmail.com>
2023-01-07 22:02:54 -0800

    test

--- 0x00-python-hello_world/6-print_comb3.py
+++ 0x00-python-hello_world/6-print_comb3.py
@@ -3,7 +3,7 @@ x = 1
 for i in range(0, 10):
     for x in range(i+1, 10):
         if (i != 8):
-            print("{one}{two}, ".format(one = i, two = x), end='')
+            print("{one}{two}, ".format(one=i, two=x), end='')
         elif (i == 8):
-            print("{one}{two}".format(one = i, two = x))
+            print("{one}{two}".format(one=i, two=x))
     x = x + 1
