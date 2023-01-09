6f0cbab21c9123263fae4bd093468b473f7cb6fb
Colan Worstell <Cworstellprogramming@gmail.com>
2023-01-08 19:22:35 -0800

    test1

--- 0x01-python-import_modules/0-add.py
+++ 0x01-python-import_modules/0-add.py
@@ -3,4 +3,4 @@ if __name__ == '__main__':
     from add_0 import add
     a = 1
     b = 2
-    print('{one} + {two} = {results}'.format(one=a, two=b, results=add(a,b)))
+    print('{one} + {two} = {results}'.format(one=a, two=b, results=add(a, b)))
