92cfc0babd7544fb0750ef500f573f27758a1612
Colan Worstell <Cworstellprogramming@gmail.com>
2023-01-08 20:11:12 -0800

    ??

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
     for i in range(1, amount):
         val = sys.argv[i]
         if (amount == 1):
-            print('{} argument:\n{}: {}'.format(amount, i, val))
+            print('{} argument:\n{}: {}'.format(amount-1, i, val))
         elif (amount > 1):
-            print('{} arguments:\n{}: {}'.format(amount, i, val))
+            print('{} arguments:\n{}: {}'.format(amount-1, i, val))
         else:
-            print('{} arguments.\n'.format(amount))
+            print('{} arguments.\n'.format(amount-1))
