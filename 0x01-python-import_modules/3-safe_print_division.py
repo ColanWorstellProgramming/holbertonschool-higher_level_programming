416c06f0962726de2515a99126a897b0995d074d
Colan Worstell <Cworstellprogramming@gmail.com>
2023-01-08 22:11:54 -0800

    try

--- 0x01-python-import_modules/3-safe_print_division.py
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -4,6 +4,6 @@ def safe_print_division(a, b):
     try:
         x = a / b
     except:
-        print('{}'.format('None'))
+        raise print('{}'.format('None'))
     finally:
         print('Inside result: {}'.format(x))
