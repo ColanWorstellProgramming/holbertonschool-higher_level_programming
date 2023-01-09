01c643be9435933a8ad7450fe0a9f8af26dc263a
Colan Worstell <Cworstellprogramming@gmail.com>
2023-01-08 20:20:05 -0800

    ??

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -7,7 +7,7 @@ if (amount == 1):
     print('{} argument:\n'.format(amount-1))
 elif (amount > 1):
     print('{} arguments:\n'.format(amount-1))
-else:
+elif (amount == 0):
     print('{} arguments.\n'.format(amount-1))
 
     for i in range(1, amount):
