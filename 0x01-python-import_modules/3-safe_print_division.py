4446178e73efe72145e70f35076842f70ba31ae6
Colan Worstell <Cworstellprogramming@gmail.com>
2023-01-08 22:13:16 -0800

    try

--- 0x01-python-import_modules/3-safe_print_division.py
+++ 0x01-python-import_modules/3-safe_print_division.py
@@ -3,7 +3,7 @@ def safe_print_division(a, b):
     x = 0
     try:
         x = a / b
-    except:
-        raise print('{}'.format('None'))
+    except ZeroDivisionError:
+        print('{}'.format('None'))
     finally:
         print('Inside result: {}'.format(x))
