2f91987d1be3056f93f702a90299dd9ea008be6d
Colan Worstell <Cworstellprogramming@gmail.com>
2023-01-16 21:54:14 -0800

    fix

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -2,9 +2,12 @@ def roman_to_int(roman_string):
     val = {'I':1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
     var = 0
 
+    if (isinstance(roman_string, str) != True or roman_string == None):
+        return(0)
+
     for i in range(len(roman_string)):
         if i+1 < len(roman_string) and val[roman_string[i]] < val[roman_string[i+1]]:
             var -= val[roman_string[i]]
         else:
             var += val[roman_string[i]]
-    return var
+    return (var)
