fec350e73e65015dac77f5115b7a4bf729126ded
Colan Worstell <Cworstellprogramming@gmail.com>
2023-01-16 16:29:20 -0800

    5-test6

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -4,4 +4,5 @@ def no_c(my_string):
     for i in range(0, len(my_string)):
         if (ord(my_string[i]) == 67 or ord(my_string[i]) == 99):
             text = text[0:i] + text[i+1:]
-    return text
+    my_string = text
+    return my_string
