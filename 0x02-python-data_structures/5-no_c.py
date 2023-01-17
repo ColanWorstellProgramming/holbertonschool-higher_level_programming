a8f5b84d124316822b961464d6554ab0e8e439d8
Colan Worstell <Cworstellprogramming@gmail.com>
2023-01-16 16:30:12 -0800

    5-test7

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    text = my_string
-    for i in range(0, len(my_string)):
-        if (ord(my_string[i]) == 67 or ord(my_string[i]) == 99):
-            text = text[0:i] + text[i+1:]
-    my_string = text
-    return my_string
+    return ''.join(char for char in my_string if char not in 'cC')
