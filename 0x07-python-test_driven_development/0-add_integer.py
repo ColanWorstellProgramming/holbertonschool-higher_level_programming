2da0903bd3b27bfb4b639ed56abfb7e7620620ba
Colan Worstell <Cworstellprogramming@gmail.com>
2023-01-23 18:35:17 -0800

    forgotcomments

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""add ints with test files"""
+
+
 def add_integer(a, b=98):
+    """function to add ints of integer type ONLY - can convert floats"""
     if (a is not int or float):
         raise TypeError('a must be an integer')
     elif (b is not int or float):
