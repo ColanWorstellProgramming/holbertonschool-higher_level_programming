5cf071ef5bb0c481337f68353901f44979a0afce
Colan Worstell <Cworstellprogramming@gmail.com>
2023-01-23 18:44:02 -0800

    logicerror

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,14 +4,9 @@
 
 def add_integer(a, b=98):
     """function to add ints of integer type ONLY - can convert floats"""
-    if (a is not int or float):
+    if (not isinstance(a, int) and not isinstance(a, float)):
         raise TypeError('a must be an integer')
-    elif (b is not int or float):
+    if (not isinstance(b, int) and not isinstance(b, float)):
         raise TypeError('b must be an integer')
 
-    if (b is float):
-        b = int(b)
-    if (a is float):
-        a = int(a)
-
-    return a + b
+    return int(a) + int(b)
