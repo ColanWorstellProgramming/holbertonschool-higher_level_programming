f7ad9f7f846bb706b43924829b039fec8ca38fac
Colan Worstell <Cworstellprogramming@gmail.com>
2023-01-31 18:49:44 -0800

    attributes

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""rectangle"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """rectangle class"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """set width"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """set height"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """x"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """set x"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """y"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """set y"""
+        self.__y = value
