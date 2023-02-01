18d37a099f2c34d3a2f2993d1812d551807ecf57
Colan Worstell <Cworstellprogramming@gmail.com>
2023-01-31 17:50:23 -0800

    7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""json file"""
+import sys
+load = __import__('6-load_from_json_file').load_from_json_file
+save = __import__('5-save_to_json_file').save_to_json_file
+
+
+if __name__ == "__main__":
+    """compare"""
+    try:
+        load_this = load("add_item.json")
+    except FileNotFoundError:
+        load_this = []
+    load_this.extend(sys.argv[1:])
+    save(load_this, "add_item.json")
