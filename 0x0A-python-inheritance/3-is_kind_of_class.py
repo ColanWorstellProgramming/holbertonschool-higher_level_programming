df866d015b4baa16e92eb7b9b44e218f394c340e
Colan Worstell <Cworstellprogramming@gmail.com>
2023-01-31 16:39:34 -0800

    2

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -4,7 +4,7 @@
 
 def is_kind_of_class(obj, a_class):
     """kind of function"""
-    if (obj is isinstance(a_class)):
+    if (isinstance(obj, a_class)):
         return True
     else:
         return False
