ab8531898727bd1e1ba706926158506cde46f63c
Colan Worstell <Cworstellprogramming@gmail.com>
2023-01-31 18:15:57 -0800

    test

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -3,10 +3,14 @@
 
 
 def pascal_triangle(n):
-    """tri function"""
-    if n == 1:
-        return[1]
-    if n == 2:
-        return[1, 1]
-    if n > 2:
-        pass
+    result = []
+    for x in range(n):
+        if x == 0:
+            result.append([1])
+        else:
+            row = [1]
+            for y in range(1, x):
+                row.append(result[x-1][y-1] + result[x-1][y])
+            row.append(1)
+            result.append(row)
+    return result
