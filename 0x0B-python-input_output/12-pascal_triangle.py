f0a07968f7122d7d0fa939ae3aec7e14e1326e7c
Colan Worstell <Cworstellprogramming@gmail.com>
2023-01-31 18:11:05 -0800

    test

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""triangle file"""
+
+
+def pascal_triangle(n):
+    """tri function"""
+    if (n <= 0):
+        return[]
+    if (n == 1):
+        return[1]
+    if (n == 2):
+        return[1, 1]
+    if (n > 2):
+        pass
