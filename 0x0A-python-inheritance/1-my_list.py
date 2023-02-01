4e684cb3804542c8d64451ca86ad431b09a8572c
Colan Worstell <Cworstellprogramming@gmail.com>
2023-01-31 16:22:20 -0800

    test

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,8 +4,5 @@
 
 class MyList(list):
     """function in class to print sorted list"""
-    def __init__(self):
-        super().__init__()
-
     def print_sorted(self):
         print(sorted(self))
