29042ffef9032e1c9f610c705eda81f3b76d98f9
Colan Worstell <Cworstellprogramming@gmail.com>
2023-02-12 10:29:10 -0800

    create

---
+++ 0x00-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- create table
+CREATE TABLE IF NOT EXISTS second_table;
+INSERT INTO second_table(int id, VARCHAR(256) name, int score) VALUES (1, 'John', 10);
+INSERT INTO second_table(int id, VARCHAR(256) name, int score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table(int id, VARCHAR(256) name, int score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table(int id, VARCHAR(256) name, int score) VALUES (4, 'George', 8);
