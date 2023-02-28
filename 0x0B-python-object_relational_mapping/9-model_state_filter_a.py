e26fed231162f37dcfa84e6514c01a79aeab32aa
Colan Worstell <Cworstellprogramming@gmail.com>
2023-02-28 14:46:39 -0800

    order

--- 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
@@ -14,6 +14,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for state in session.query(State).order_by(State.id)
+    for state in session.query(State).order_by(State.id):
         if state.name.contains('a'):
             print("{}: {}".format(state.id, state.name))
