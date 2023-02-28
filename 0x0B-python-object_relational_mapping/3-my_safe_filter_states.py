dbec9379794742fe184ce9b99833aa2ecbf25a6c
Colan Worstell <Cworstellprogramming@gmail.com>
2023-02-27 17:00:56 -0800

    binary

--- 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,7 +9,8 @@ def selectNstate():
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name='{}'".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name \
+                LIKE BINARY'{}'".format(sys.argv[4]))
     y = cur.fetchall()
     for x in y:
         print(x)
