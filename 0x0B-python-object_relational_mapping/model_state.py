0dd64339cec938aa14aaee2c0aa237ccc46ca4d5
Colan Worstell <Cworstellprogramming@gmail.com>
2023-02-27 17:50:38 -0800

    error

--- 0x0B-python-object_relational_mapping/model_state.py
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -9,4 +9,4 @@ class State(Base):
     """state class"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(128), nullable=False)\
+    name = Column(String(128), nullable=False)
