9675970fb16e7978086d54d044dbef8546da0ad9
Colan Worstell <Cworstellprogramming@gmail.com>
2023-02-27 16:15:41 -0800

    1

--- 0x0B-python-object_relational_mapping/0-select_states.sql
+++ 0x0B-python-object_relational_mapping/0-select_states.sql
@@ -1,4 +1,3 @@
--- Create states table in hbtn_0e_0_usa with some data
 CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
 USE hbtn_0e_0_usa;
 CREATE TABLE IF NOT EXISTS states ( 
@@ -7,4 +6,3 @@ CREATE TABLE IF NOT EXISTS states (
 	    PRIMARY KEY (id)
 	);
 	INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
-
---
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""connect to MySQLdb"""
+import MySQLdb
+import sys
+
+
+def selectstate():
+    """SQL Selection Function"""
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE LEFT(name, 1) LIKE 'N' ORDER BY id")
+    y = cur.fetchall()
+    for x in y:
+        print(x)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    selectstate()
