b6eda69d879d85cc7a70675541b03b1409093830
Colan Worstell <Cworstellprogramming@gmail.com>
2023-02-27 16:04:43 -0800

    0

---
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""connect to MySQLdb"""
+import MySQLdb
+import sys
+
+def selectstate():
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id")
+    y = cur.fetchall()
+    for x in y:
+        print(x)
+
+if __name__ == "__main__":
+    selectstate()
