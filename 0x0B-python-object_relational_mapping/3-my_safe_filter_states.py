b609005e21e9fe13c638245542169698e5d803f0
Colan Worstell <Cworstellprogramming@gmail.com>
2023-02-27 16:59:46 -0800

    binary

---
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""connect to MySQLdb"""
+import MySQLdb
+import sys
+
+
+def selectNstate():
+    """SQL Selection Function"""
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name='{}'".format(sys.argv[4]))
+    y = cur.fetchall()
+    for x in y:
+        print(x)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    selectNstate()
