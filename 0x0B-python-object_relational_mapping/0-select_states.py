cd761aff79564995923a62ab1cfdb394e574aca0
Colan Worstell <Cworstellprogramming@gmail.com>
2023-02-27 16:06:38 -0800

    adding

--- 0x0B-python-object_relational_mapping/0-select_states.py
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -4,6 +4,7 @@ import MySQLdb
 import sys
 
 def selectstate():
+    """SQL Selection Function"""
     db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id")
