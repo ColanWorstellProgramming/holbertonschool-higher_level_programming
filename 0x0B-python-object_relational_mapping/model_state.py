c842d988641345979766670a13e8d81bb4f748dd
Colan Worstell <Cworstellprogramming@gmail.com>
2023-02-28 14:19:12 -0800

    7

--- 0x0B-python-object_relational_mapping/model_state.py
+++ 0x0B-python-object_relational_mapping/model_state.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
 """model state"""
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
+import sys
+from sqlalchemy import create_engine
+from model_state import State
+from sqlalchemy.orm import sessionmaker
 
 
-class State(Base):
-    """state class"""
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
-    name = Column(String(128), nullable=False)
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
