4273d856969fff0387273dc150b72d3f039e70ba
Colan Worstell <Cworstellprogramming@gmail.com>
2023-02-27 17:10:23 -0800

    name

--- 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -10,7 +10,7 @@ def selectNstate():
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name \
-                LIKE BINARY'{}'".format(sys.argv[4]))
+                LIKE BINARY %(name)s", {sys.argv[4]})
     y = cur.fetchall()
     if y is None:
         return False
