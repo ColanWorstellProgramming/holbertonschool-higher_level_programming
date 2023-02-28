3912672ce69bd14606828fc25e113616633cdba8
Colan Worstell <Cworstellprogramming@gmail.com>
2023-02-27 16:26:13 -0800

    test

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -9,7 +9,7 @@ def selectNstate():
     db = MySQLdb.connect(host='localhost', port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE LEFT(name, 1) LIKE 'N'")
+    cur.execute("SELECT * FROM states WHERE LEFT(name, 1) LIKE '^N'")
     y = cur.fetchall()
     for x in y:
         print(x)
