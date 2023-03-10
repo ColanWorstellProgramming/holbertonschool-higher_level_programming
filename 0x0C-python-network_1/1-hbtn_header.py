b075eda64b6d38a8a59ce6a7bec6d37032920a33
Colan Worstell <Cworstellprogramming@gmail.com>
2023-03-09 22:50:07 -0800

    3rd

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -4,7 +4,7 @@ import urllib.request
 import sys
 
 if __name__ == "__main__":
-    req = urllib.request.Request('https://intranet.hbtn.io')
+    req = urllib.request.Request(sys.argv[1])
     with urllib.request.urlopen(req) as response:
         html = response.read()
         print(response.headers['X-Request-Id'])
