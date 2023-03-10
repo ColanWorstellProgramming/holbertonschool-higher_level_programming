19ee4c15dceb9ec35c1e242f636b2e0165cae70a
Colan Worstell <Cworstellprogramming@gmail.com>
2023-03-09 18:58:32 -0800

    nooutput

---
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""HBTN Status"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
