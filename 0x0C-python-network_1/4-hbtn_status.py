e0eccf2dc4a528393d7550c9aa211f7837b9dfb9
Colan Worstell <Cworstellprogramming@gmail.com>
2023-03-09 23:43:09 -0800

    module

--- 0x0C-python-network_1/4-hbtn_status.py
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """HBTN Status"""
-import urllib.request
+import requests
 
 
 if __name__ == "__main__":
-    req = urllib.request.Request('https://intranet.hbtn.io/status')
-    with urllib.request.urlopen(req) as response:
-        html = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
+    req = requests.Request('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(req.text)))
+    print("\t- content: {}".format(req.text))
