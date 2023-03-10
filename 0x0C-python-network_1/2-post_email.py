78f5ccb66d810d990961dc24c7b2c2f4d4e70529
Colan Worstell <Cworstellprogramming@gmail.com>
2023-03-09 23:21:37 -0800

    2

--- 0x0C-python-network_1/2-post_email.py
+++ 0x0C-python-network_1/2-post_email.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """HBTN Status"""
-from urllib import request, pasrse
+from urllib import request, parse
 import sys
 
 if __name__ == "__main__":
-    req = request.Request(sys.argv[1], sys.argv[2])
-    with request.urlopen(req) as response:
-        html = response.read()
-        print(response.headers['X-Request-Id'])
+    req = sys.argv[1]
+    email = {'email': sys.argv[2]}
+    par = parse.urlencode(email).encode('utf-8')
+    bef = request.Request(req, par)
+    with request.urlopen(bef) as response:
+        html = response.read().decode('utf-8')
+        print(html)
