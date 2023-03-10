14fbdae7d4e79be2fd04d214ed6df87502522ccf
Colan Worstell <Cworstellprogramming@gmail.com>
2023-03-09 23:41:36 -0800

    4

---
+++ 0x0C-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""HBTN Status"""
+import urllib.request
+
+
+if __name__ == "__main__":
+    req = urllib.request.Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen(req) as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
