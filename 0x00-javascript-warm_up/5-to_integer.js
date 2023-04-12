9605b66e0b98bbf6dbeb1f92f3cfa3563c6e3585
Colan Worstell <Cworstellprogramming@gmail.com>
2023-04-11 17:34:42 -0700

    fix

---
+++ 0x00-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+if (process.argv[2] === undefined) {
+  console.log('Not a number');
+} else {
+  const arg = process.argv[2];
+  const integerArg = parseInt(arg);
+
+  if (isNaN(integerArg)) {
+    console.log('Not a number');
+  } else {
+    console.log(`My number: ${integerArg}`);
+  }
+}
