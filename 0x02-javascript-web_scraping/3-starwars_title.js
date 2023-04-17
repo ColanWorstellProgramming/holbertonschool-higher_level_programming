ff916e1d006cec7fd78ed7618d17ead38d25b916
Colan Worstell <Cworstellprogramming@gmail.com>
2023-04-16 23:53:18 -0700

    starwars

---
+++ 0x02-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const sw = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(sw, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
